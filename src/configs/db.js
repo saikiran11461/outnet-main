35a73473988103ea758c70cab837174cc49499d7
saikiran11461 <70562454+saikiran11461@users.noreply.github.com>
2022-03-31 17:21:22 +0530

    Delete db.js

--- src/configs/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = () => {
-  return mongoose.connect("mongodb+srv://saikiran11471:saikiran11471@cluster0.4h8lk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-};
